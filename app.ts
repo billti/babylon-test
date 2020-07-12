fda35adcb53b55f8e2a5811e59c55ce5579d3050
Bill Ticehurst <billti@microsoft.com>
2020-07-12 14:47:22 -0700

    Switched to named imports

--- app.ts
+++ app.ts
@@ -1,22 +1,23 @@
-import * as BABYLON from "babylonjs"
+// Below is removed by WebPack during the build. It's available as an 'external'
+import {Engine, Scene, MeshBuilder, ArcRotateCamera, HemisphericLight, Vector3, Mesh} from "babylonjs"
 
 var canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
-var engine: BABYLON.Engine = new BABYLON.Engine(canvas!, true);
+var engine: Engine = new Engine(canvas!, true);
 
-function createScene(): BABYLON.Scene {
-    var scene: BABYLON.Scene = new BABYLON.Scene(engine);
+function createScene(): Scene {
+    var scene: Scene = new Scene(engine);
 
-    var camera: BABYLON.ArcRotateCamera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, BABYLON.Vector3.Zero(), scene);
+    var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
     camera.attachControl(canvas, true);
 
-    var light1: BABYLON.HemisphericLight = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
+    var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
 
-    var sphere: BABYLON.Mesh = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
+    var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
 
     return scene;
 }
 
-var scene: BABYLON.Scene = createScene();
+var scene: Scene = createScene();
 
 engine.runRenderLoop(() => {
     scene.render();
